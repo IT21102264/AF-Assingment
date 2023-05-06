a79c33929d726fae272baf672be7ddf2df3e6cc7
Nisura <it21102264@my.sliit.lk>
2023-05-07 00:48:01 +0530

    adding css part

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -24,6 +24,9 @@ export default function SignupPage() {
           flexDirection: "row",
           alignItems: "center",
           padding: "20px",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
+          boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)",
         }}
       >
         <div className="login-c">
