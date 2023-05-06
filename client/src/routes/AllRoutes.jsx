d4c9979c36c481470255ddbcc7413a7c87cb36aa
Nisura <it21102264@my.sliit.lk>
2023-05-06 13:01:39 +0530

    adding logout function

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,24 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "../pages/AboutPage";
-import HomePage from "../pages/HomePage";
 import ShopPage from "../pages/ShopPage";
+import AdminPage from "../pages/AdminPage";
+import { Products } from "../pages/Products";
+import { AddProducts } from "../pages/AddProduct";
+import PayPage from "../pages/PayPage";
+import AdminRoute from "./AdminRoute";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
-import AdminPage from "../pages/AdminPage";
-
-import AdminRoute from "./AdminRoute";
+import HomePage from "../pages/HomePage";
+import Cart from "../pages/Cart";
 
 export default function Allroutes() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          
-            <HomePage></HomePage>
-          
-        }
-      ></Route>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/shop" element={<ShopPage />} />
       <Route path="/about" element={<AboutPage></AboutPage>}></Route>
       <Route path="/login" element={<LoginPage></LoginPage>}></Route>
@@ -31,6 +27,11 @@ export default function Allroutes() {
           </AdminRoute>
         }
       />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/admin/products" element={<Products />} />
+      <Route path="/admin/addProducts" element={<AddProducts />} />
+      <Route path="/cart" element={<Cart></Cart>} />
+      <Route path="/paypage" element={<PayPage></PayPage>} />
     </Routes>
   );
 }
