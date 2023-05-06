0782339f3bb0438d063fad8f26a268116653fa08
Binuja <it21104176@my.sliit.lk>
2023-05-06 09:53:48 +0530

    new file:   client/src/Pages/PayPage.css 	modified:   client/src/Pages/PayPage.js

--- client/src/Pages/PayPage.js
+++ client/src/Pages/PayPage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useState } from "react";
 import axios from "axios";
+import "./PayPage.css";
 import { Box, Input, Button, VStack, Flex } from "@chakra-ui/react";
+import Header from '../components/Header';
 
 
 export default function PayPage() {
@@ -21,7 +23,7 @@ export default function PayPage() {
             cvv: cvv,
           }
           
-          axios.post("http://localhost:5000/payment/add", newPayment).then(() => {
+          axios.post("http://localhost:4000/payment/add", newPayment).then(() => {
             alert("Sent data");
           }).catch(err => {
             console.log(err);
@@ -30,51 +32,38 @@ export default function PayPage() {
       };
 
 return (
-    <Box mt="60px">
-    <Flex gap={6} alignItems={"center"} direction={"column"} w="40%" m="auto">
-    <div className="row">
-        <div className="col-md-4 mb-3">
-        <label for="validationname">Card Holdername</label>
-            <Input
-                value={cardholdername}
-                placeholder="cardholdername"
-                required
-                onChange={(e) => setCardHolderName(e.target.value)}
-            ></Input>
-        </div>
-        <div className="col-md-4 mb-3">
-        <label for="validationnumber">Card Number</label>
-            <Input
-                value={cardnumber}
-                placeholder="cardNumber"
-                required
-                onChange={(e) => setCardNumber(e.target.value)}
-            ></Input>
-        </div>
-        <div className="col-md-4 mb-3">
-        <label for="validationdate">Expire Date</label>
-            <Input
-                type={"date"}
-                value={expdate}
-                placeholder="expiry"
-                required
-                onChange={(e) => setExpiry(e.target.value)}
-            ></Input>
-        </div>
-        <div className="col-md-4 mb-3">
-        <label for="validationcvv">Cvv No</label>
-            <Input
-                value={cvv}
-                placeholder="cvv"
-                minLength="3"
-                maxLength="3"
-                required
-                onChange={(e) => setCvv(e.target.value)}
-            ></Input>
-        </div>
-    </div>
-     <Button onClick={sentData}>PayNow</Button>
-    </Flex>
-  </Box>
+  <div>
+    <Header />
+    <div class="container">
+    <center><div class="title">Bill Payment Form</div></center>
+    <hr></hr>
+      <div class="content">
+            <form action="adver.php" method="post">
+              <div class="user-details">
+                <div class="input-box">
+                  <label class="details">Card Holder Name</label>
+                  <input type="text" placeholder="Enter Card Holder name"  onChange={(e) => setCardHolderName(e.target.value)} required />
+                </div>
+                <div class="input-box">
+                  <label class="details">Card Number</label>
+                  <input type="number" placeholder="Enter card number"  onChange={(e) => setCardNumber(e.target.value)} required />	  
+                </div>
+                <div class="input-box">
+                  <label class="details">Expire date</label>
+                  <input type="date"  placeholder="Enter Exp date"  onChange={(e) => setExpiry(e.target.value)} required />
+                </div>
+                <div class="input-box">
+                  <label class="details">Cvv</label>
+                  <input type="number" placeholder="Enter cvv no"  onChange={(e) => setCvv(e.target.value)} required />
+                </div><br/>
+
+              </div>
+              <center><Button onClick={sentData}>Process to checkout</Button></center>
+          </form>
+      </div>
+		</div>
+
+  </div>
+
   );
 }
